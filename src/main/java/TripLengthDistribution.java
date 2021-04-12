d5b31c1af156a1a10ef85c30a627fced487a3e9e
simiaolin <simiao.lin@student.kuleuven.be>
2021-04-12 20:09:14 +0200

    [fix] fix the bug of using in for longtitude.
    1. two types of distance definition.

--- src/main/java/TripLengthDistribution.java
+++ src/main/java/TripLengthDistribution.java
@@ -34,17 +34,27 @@ public class TripLengthDistribution {
             Double startLong = Double.valueOf(columns[3]);
             Double endLat = Double.valueOf(columns[5]);
             Double endLong = Double.valueOf(columns[6]);
-            distances.add(getDistance(startLat, startLong, endLat, endLong));
+            distances.add(getSphericalProjectionDistance(startLat, startLong, endLat, endLong));
         }
         return distances;
     }
 
     //todo investigate other two
-    public static Double getDistance(double startLat, double startLong, double endLat, double endLong) {
+    public static Double getPolarCoordinateDistance(double startLat, double startLong, double endLat, double endLong) {
         Double startColatitudeInRadian = getColatitudeInRadian(startLat);
         Double endColatitudeInRadian = getColatitudeInRadian(endLat);
-        Double deltaLong = endLong - startLong;
-        return R * Math.sqrt(Math.pow(startColatitudeInRadian, 2) + Math.pow(endColatitudeInRadian, 2) - 2 * startColatitudeInRadian * endColatitudeInRadian * Math.cos(deltaLong));
+        Double deltaLong = (endLong - startLong) * Math.PI / 180;
+        return R * Math.sqrt(Math.pow(startColatitudeInRadian, 2) + Math.pow(endColatitudeInRadian, 2)
+                - 2 * startColatitudeInRadian * endColatitudeInRadian * Math.cos(deltaLong)) * 1000;
+    }
+
+   public static Double getSphericalProjectionDistance(double startLat, double startLong, double endLat, double endLong) {
+        Double startColatitudeInRadian = getColatitudeInRadian(startLat);
+        Double endColatitudeInRadian = getColatitudeInRadian(endLat);
+        Double deltaLat = (endLat - startLat) ;
+        Double deltaLong = (endLong - startLong) ;
+        Double midLat = (startLat + endLat) / 2 * Math.PI / 180;
+        return R * Math.PI * Math.sqrt(Math.pow(deltaLat, 2) + Math.pow(Math.cos(midLat) * deltaLong, 2)) * 1000 / 180;
     }
 
     public static Double getColatitudeInRadian(Double degree) {
