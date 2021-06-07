92912d9c73cd4c938143b04eb0e20511a938ead3
simiaolin <simiao.lin@student.kuleuven.be>
2021-06-07 09:51:23 +0200

    [add] add comment on Exercise1

--- src/main/java/Exercise1.java
+++ src/main/java/Exercise1.java
@@ -12,6 +12,15 @@ import java.util.Map;
 
 public class Exercise1 {
 
+    /** To compute the trip distance distribution, two ways are provided -- the simple algorithm, and the spark solution.
+     * @param args     Three params are needed. The meaning of the params are shown below.
+     *                 1. whether the job is run on spark or not.
+     *                      - spark -> run on spark
+     *                      - simple -> run on normal java
+     *                 2. input file path
+     *                 3. output file path
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         if (args.length < 3) {
             throw new Exception("please specify 3 arguments \n" +
@@ -36,13 +45,29 @@ public class Exercise1 {
 
 }
 
+/**
+ * SimpleTripDistribution is a simple program to compute the trip distance distribution.
+ */
 class SimpleTripDistribution{
+
+    /**
+     * Given a file recording trip information, write out the trip distance distribution.
+     * @param input         path of file containing trip records
+     * @param output        path of file to write out the trip distance distribution
+     * @throws IOException
+     */
     public static void execTripDistribution(String input, String output) throws IOException {
         List<String> lines = readTripFile(input);
         HashMap<Integer, Integer> distribution = getDistanceDistribution(lines);
         writeTripLength(output, distribution);
     }
 
+    /**
+     * Read lines from a file.
+     * @param input   path of input file
+     * @return        a list of lines in the input file
+     * @throws IOException
+     */
     public static List<String> readTripFile(String input) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(input));
         List<String> lines = new ArrayList<>();
@@ -55,6 +80,12 @@ class SimpleTripDistribution{
         return lines;
     }
 
+    /**
+     * Write the trip distance distribution to the output file
+     * @param output         path of output file
+     * @param distribution   a map of trip distance distribution, where the KEY is the trip distance, and the VALUE is the count of the distance.
+     * @throws IOException
+     */
     public static void writeTripLength(String output, Map<Integer, Integer> distribution) throws IOException {
         BufferedWriter bw = new BufferedWriter(new FileWriter(output));
         for (Map.Entry<Integer, Integer> dtb : distribution.entrySet()) {
@@ -64,7 +95,11 @@ class SimpleTripDistribution{
         bw.close();
     }
 
-
+    /**
+     * Get the trip distance distribution.
+     * @param lines      a list of trip records
+     * @return           a map of trip distance distribution, where the KEY is the trip distance, and the VALUE is the count of the distance.
+     */
     public static HashMap<Integer, Integer> getDistanceDistribution(List<String> lines) {
         HashMap<Integer, Integer> distribution = new HashMap<>();
         for (String line : lines) {
@@ -90,7 +125,18 @@ class SimpleTripDistribution{
     }
 }
 
+
+/**
+ * SparkTripDistribution is a spark program to compute the trip distance distribution.
+ */
 class SparkTripDistribution {
+
+    /**
+     * Given a file recording trip information, write out the trip distance distribution.
+     * @param input         path of file containing trip records
+     * @param output        path of file to write out the trip distance distribution
+     * @throws IOException
+     */
     public static void execTripDistribution(String input, String output) throws IOException {
         SparkConf conf = new SparkConf().setAppName("TripDistribution").setMaster("local[*]");
         JavaSparkContext context = new JavaSparkContext(conf);
@@ -104,7 +150,7 @@ class SparkTripDistribution {
                 .mapToPair(tripAndSpeedTuple -> new Tuple2<>(tripAndSpeedTuple._1, 1)); // <distance, speed> -> <distance,1>
 
 
-        //count on every distance
+        //count the occurrence of every trip distance.
         JavaPairRDD<Integer, Integer> distanceDistributionRDD = distancesRDD.reduceByKey(Integer::sum);
 
         //write the result to file
@@ -112,6 +158,12 @@ class SparkTripDistribution {
         context.stop();
     }
 
+    /**
+     * Write the trip distribution to output file.
+     * @param output           the output filename
+     * @param distribution     a list of (trip distance, count of the distance) tuples.
+     * @throws IOException
+     */
     public static void writeTripLength(String output,   List<Tuple2<Integer, Integer>> distribution) throws IOException {
         BufferedWriter bw = new BufferedWriter(new FileWriter(output));
         distribution.forEach(e -> {
@@ -129,10 +181,17 @@ class SparkTripDistribution {
 
 }
 
-
+/**
+ * DistanceUtilOne is a utility class for exercise one.
+ */
 class DistanceUtilOne {
     private static final double R = 6371.009;
 
+    /**
+     * Given a line of trip record, calculate the trip distance and the trip speed.
+     * @param line   A line of trip record
+     * @return       A (trip distance, trip speed) tuple
+     */
     public static Tuple2<Integer, Double> getDistanceAndSpeedTuple(String line) {
         String[] trip = line.split(" ");
         Double distance = DistanceUtilOne.getSphericalProjectionDistance(
@@ -142,6 +201,14 @@ class DistanceUtilOne {
         return new Tuple2<>((int) Math.round(distance), speed);
     }
 
+    /**
+     * To get the sphericalProjectionDistance.
+     * @param startLat      The latitude of the start position
+     * @param startLong     The longtitude of the start position
+     * @param endLat        The latitude of the end position
+     * @param endLong       The longtitude of the end position
+     * @return              Tje computed sphericalProjectionDistance
+     */
     public static Double getSphericalProjectionDistance(double startLat, double startLong, double endLat, double endLong) {
         Double deltaLat = (endLat - startLat);
         Double deltaLong = (endLong - startLong);
@@ -149,6 +216,12 @@ class DistanceUtilOne {
         return R * Math.PI * Math.sqrt(Math.pow(deltaLat, 2) + Math.pow(Math.cos(midLat) * deltaLong, 2)) * 1000 / 180;
     }
 
+    /**
+     * To get the speed of a trip given distance and trip time.
+     * @param distance       The distance of the trip
+     * @param tripTime       The time spent on the trip
+     * @return               The spped of the trip
+     */
     public static Double getSpeed(double distance, double tripTime) {
         double speed = 3.6 * distance / tripTime;
         return speed;
